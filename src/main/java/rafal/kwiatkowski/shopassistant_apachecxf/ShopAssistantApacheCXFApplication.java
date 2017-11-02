1c080488e94dbf0f40e2b2e52fd095a5a4a99c51
Rafał Kwiatkowski <raf.kwiatkowski93@gmail.com>
2017-11-02 22:32:10 +0100

    initial commit

---
+++ src/main/java/rafal/kwiatkowski/shopassistant_apachecxf/ShopAssistantApacheCXFApplication.java
@@ -0,0 +1,12 @@
+package rafal.kwiatkowski.shopassistant_apachecxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAssistantApacheCXFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopAssistantApacheCXFApplication.class, args);
+	}
+}
---
+++ src/test/java/rafal/kwiatkowski/shopassistant_apachecxf/ShopAssistantApacheCXFApplicationTests.java
@@ -0,0 +1,16 @@
+package rafal.kwiatkowski.shopassistant_apachecxf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopAssistantApacheCXFApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
